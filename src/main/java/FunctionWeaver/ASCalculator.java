280d9a481dd7d4e247ab21405dd05dd3cf4f98f7
Name <me@email.com>
2021-01-19 04:24:04 -0600

    added basic calculations +, -, /, *

--- src/main/java/FunctionWeaver/ASCalculator.java
+++ src/main/java/FunctionWeaver/ASCalculator.java
@@ -3,16 +3,41 @@ package FunctionWeaver;
 public class ASCalculator {
     public ASCalculator() {}
     StringBuilder equation;
+    String left;
+    String right;
     int stringLength;
+    int substringLStart;
+    int substringLEnd;
+    int substringRStart;
+    int substringREnd;
+    double firstValue;
+    double secondValue;
+    double resultValue;
 
     public StringBuilder add (StringBuilder equation) {
         System.out.println(equation + " class for addition evaluation is being reached");
-        // some calculation code will go here in the future
+
+        substringLStart = 0;
+        substringREnd = equation.length();
+        for (int i = 1; i < equation.length(); i++)
+        {   if (equation.charAt(i) == '+')
+            {   substringLEnd = i;
+                substringRStart = i + 1;
+                break;
+            }
+
+        }
+        left = equation.substring(substringLStart, substringLEnd);
+        right = equation.substring(substringRStart, substringREnd);
+        firstValue = Double.parseDouble(left);
+        secondValue = Double.parseDouble(right);
+        resultValue = firstValue + secondValue;
+
         stringLength = equation.length();
         for (int i = 0; i < stringLength; i++)
         equation.delete(0, 1);
         
-        equation.append("1");
+        equation.append(new Double(resultValue).toString());
         System.out.println(equation.toString() + " leaving addition class");
 
         return equation;
@@ -20,12 +45,28 @@ public class ASCalculator {
 
     public StringBuilder subtract (StringBuilder equation) {
         System.out.println(equation + " class for subtraction evaluation is being reached");
-        // some calculation code will go here in the future
+
+        substringLStart = 0;
+        substringREnd = equation.length();
+        for (int i = 1; i < equation.length(); i++)
+        {   if (equation.charAt(i) == '-')
+            {   substringLEnd = i;
+                substringRStart = i + 1;
+                break;
+            }
+
+        }
+        left = equation.substring(substringLStart, substringLEnd);
+        right = equation.substring(substringRStart, substringREnd);
+        firstValue = Double.parseDouble(left);
+        secondValue = Double.parseDouble(right);
+        resultValue = firstValue - secondValue;
+
         stringLength = equation.length();
         for (int i = 0; i < stringLength; i++)
         equation.delete(0, 1);
         
-        equation.append("1");
+        equation.append(new Double(resultValue).toString());
         System.out.println(equation.toString() + " leaving addition class");
 
         return equation;
--- src/main/java/FunctionWeaver/MDCalculator.java
+++ src/main/java/FunctionWeaver/MDCalculator.java
@@ -3,30 +3,70 @@ package FunctionWeaver;
 public class MDCalculator {
     public MDCalculator() {}
     StringBuilder equation;
+    String left;
+    String right;
     int stringLength;
+    int substringLStart;
+    int substringLEnd;
+    int substringRStart;
+    int substringREnd;
+    double firstValue;
+    double secondValue;
+    double resultValue;
 
     public StringBuilder multiply (StringBuilder equation) {
         System.out.println(equation + " class for multiplication evaluation is being reached");
-        // some calculation code will go here in the future
+        substringLStart = 0;
+        substringREnd = equation.length();
+        for (int i = 1; i < equation.length(); i++)
+        {   if (equation.charAt(i) == '*')
+            {   substringLEnd = i;
+                substringRStart = i + 1;
+                break;
+            }
+
+        }
+        left = equation.substring(substringLStart, substringLEnd);
+        right = equation.substring(substringRStart, substringREnd);
+        firstValue = Double.parseDouble(left);
+        secondValue = Double.parseDouble(right);
+        resultValue = firstValue * secondValue;
+
         stringLength = equation.length();
         for (int i = 0; i < stringLength; i++)
         equation.delete(0, 1);
         
-        equation.append("1");
-        System.out.println(equation.toString() + " leaving multiplication class");
+        equation.append(new Double(resultValue).toString());
+        System.out.println(equation.toString() + " leaving addition class");
 
         return equation;
     }
 
     public StringBuilder divide (StringBuilder equation) {
         System.out.println(equation + " class for division evaluation is being reached");
-        // some calculation code will go here in the future
+        
+        substringLStart = 0;
+        substringREnd = equation.length();
+        for (int i = 1; i < equation.length(); i++)
+        {   if (equation.charAt(i) == '/')
+            {   substringLEnd = i;
+                substringRStart = i + 1;
+                break;
+            }
+
+        }
+        left = equation.substring(substringLStart, substringLEnd);
+        right = equation.substring(substringRStart, substringREnd);
+        firstValue = Double.parseDouble(left);
+        secondValue = Double.parseDouble(right);
+        resultValue = firstValue / secondValue;
+
         stringLength = equation.length();
         for (int i = 0; i < stringLength; i++)
         equation.delete(0, 1);
         
-        equation.append("1");
-        System.out.println(equation.toString() + " leaving division class");
+        equation.append(new Double(resultValue).toString());
+        System.out.println(equation.toString() + " leaving addition class");
 
         return equation;
     }
