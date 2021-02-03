5019e272d370796b813e7d03b3cab13da9bbd5d2
Name <me@email.com>
2021-02-03 11:54:11 -0600

    I think i fixed it

--- src/main/java/FunctionWeaver/interfaces/Prime.java
+++ src/main/java/FunctionWeaver/interfaces/Prime.java
@@ -14,12 +14,12 @@ public interface Prime {
     }
 
     public default int putMarker2(StringBuilder equation, int i, int substringEnd) {
+        if (equation.charAt(i + 1) == '-') i += 1;
         for (int k = i + 1; k < equation.length(); k++)
         {   if (equation.charAt(k) == '0' || equation.charAt(k) == '1' || equation.charAt(k) == '2' ||
                 equation.charAt(k) == '3' || equation.charAt(k) == '4' || equation.charAt(k) == '5' ||
                 equation.charAt(k) == '6' || equation.charAt(k) == '7' || equation.charAt(k) == '8' ||
-                equation.charAt(k) == '9' || equation.charAt(k) == '.' || equation.charAt(k) == 'x' ||
-                equation.charAt(k) == '-')
+                equation.charAt(k) == '9' || equation.charAt(k) == '.' || equation.charAt(k) == 'x')
             {   substringEnd = k + 2;   }
             else break;
         }
